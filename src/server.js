58405ecb159941d19029cb7bc760f4173a1adccf
irfan khan <irfan.khan@kelltontech.com>
2023-04-12 10:56:13 +0530

    handled cors issue

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const io = require('socket.io')(http);
+const io = require('socket.io')(http, { cors: { origin: '*', } });
 const socketNamespace=io.of('/socket')
 const PORT = process.env.PORT || 3000;
 const httpStatus = require('http-status');
