16a84f9108da637664e1295a5b205fa156009b7a
Aniltmr <anil.tomar@cherishx.com>
2023-09-19 13:23:25 +0530

    added id in sse

--- src/controllers/serverSentEventsController.js
+++ src/controllers/serverSentEventsController.js
@@ -14,25 +14,35 @@ const createStoreIdConnection = catchAsync(async (req, res, next) => {
 
   if (!store_id) {
     res.write("data: " + "CLOSE" + "\n\n");
-    // res.end();
   }
 
   if (req.query.syncToPwa) {
     emitSSE(res, req.outputData)
   } else {
     const currentVersion = setInterval(async function () {
-      const result = await getCurrentVersion(res, { storeId: store_id, terminalTime: terminalTime })
-    }, 1000 * 10)
+      let time = parseInt(terminalTime);
+      time += 20000;
+      const result = await getCurrentVersion(res, { storeId: store_id, terminalTime: time })
+    }, 2000 * 10)
 
 
     const upcomingSchedules = setInterval(async function () {
-      const result = await getUpcomingSchedule(res, { storeId: store_id, terminalTime: terminalTime })
-    }, 1000 * 20)
+      let time = parseInt(terminalTime);
+      time += 40000;
+      const result = await getUpcomingSchedule(res, { storeId: store_id, terminalTime: time })
+    }, 4000 * 10)
+
+    res.on("close", () => {
+      console.log("connection closed");
+      res.end();
+      clearInterval(currentVersion)
+      clearInterval(upcomingSchedules)
+    });
   }
 
   res.on("close", () => {
     console.log("connection closed");
-    // res.end();
+    res.end();
   });
 
 })
@@ -64,6 +74,8 @@ const getUpcomingSchedule = catchAsync(async (res, input) => {
 
 
 const emitSSE = (res, data) => {
+  const id = (new Date()).toLocaleTimeString();
+  res.write('id: ' + id + '\n');
   res.write("data: " + JSON.stringify(data) + "\n\n");
   // res.end()
 };
